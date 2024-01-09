8cf9142a9886282d30823c47569709b3d021df21
Shisa Ustarkhanov <ustarkhanov.js@gmail.com>
2024-01-09 19:03:03 +0400

    Выполнил задачу Task1 из Lesson 8

---
+++ DmdevCourse/src/javabasic/lesson8/Task1.java
@@ -0,0 +1,18 @@
+package javabasic.lesson8;
+
+public class Task1 {
+  /* Задание 2
+   *  Дано целое число. Определить, является ли последняя цифра это числа - цифрой 3.
+   * */
+  public static void main(String[] args) {
+    int value = 912313;
+    if (getLastNumber(value) == 3) {
+      System.out.println("Последняя цифра числа является 3");
+    }
+  }
+
+  // Получаем последнюю цифру из числа
+  public static int getLastNumber(int number) {
+    return number % 10;
+  }
+}
