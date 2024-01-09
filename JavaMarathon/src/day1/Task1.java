3c30d89a5244e10eb7da0e191f04fddf931a7ea9
Shisa Ustarkhanov <ustarkhanov.js@gmail.com>
2024-01-09 19:26:24 +0400

    Выполнил задачу марафона День 1 - Задача 1

---
+++ JavaMarathon/src/day1/Task1.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task1 {
+    // 1. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
+    public static void main(String[] args) {
+    int i = 0;
+    while (i < 10){
+        System.out.print("JAVA ");
+        i++;
+    }
+    }
+}
