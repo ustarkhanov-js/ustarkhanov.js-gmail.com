4169781a5220ff0f29c1734f912a4009c6b62f25
Shisa Ustarkhanov <ustarkhanov.js@gmail.com>
2024-01-09 19:28:30 +0400

    Выполнил задачу марафона День 1 - Задача 5

---
+++ JavaMarathon/src/day1/Task5.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task5 {
+    // 5. Повторить задание 4, но используя цикл for.
+    public static void main(String[] args) {
+        for (int year = 1980; year <= 2020; year = year + 4){
+            System.out.println("Олимпиада " + year + " года");
+        }
+
+    }
+}
